c526ef084eca7a003cc3c085197086d3028a1e0e
chosenki <chosenki87@gmail.com>
2016-07-20 00:23:34 +0800

    approot

---
+++ ki-r/containers/AppRoot.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react'
+
+export default class AppRoot extends  Component {
+    render(){
+        return (
+            <div>
+                <h1>Hello React</h1>
+                <h2>I am AppRoot in containers folder</h2>
+            </div>
+        )
+    }
+}
--- ki-r/index.js
+++ ki-r/index.js
@@ -5,11 +5,12 @@ import { createStore,applyMiddleware } from "redux"
 import { Provider } from "react-redux"
 import logger from "redux-logger"
 import thunk from "redux-thunk"
+import AppRoot from "./containers/AppRoot"
 
 
 const middleware = process.env.NODE_ENV === "production" ? [thunk] : [thunk,logger()]
 
 render(
-    <h1>Hello React</h1>,
+    <AppRoot />,
     document.getElementById("appROOT")
 )
